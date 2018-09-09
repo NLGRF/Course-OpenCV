20dcd68d5615c436b723beb7b3a0c6df401e3c50
Non <non.devtech@gmail.com>
2018-09-10 01:39:07 +0700

    part3

---
+++ part3_Video/play_video.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture('output.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    # กำหนดค่าสีจาก frame เป็น cv2.COLOR_BGR2GRAY สีแบบเทา
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
